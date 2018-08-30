3795a520698be9ef7a29dd2e7ad8a64d78b62467
chlochlofasho <cbt0029@gmail.com>
2018-08-29 21:18:27 -0500

    formatting

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ var mymap;
 
 $(document).ready(function() {
   //access_token=pk.eyJ1IjoiY2hsb2UtbWMiLCJhIjoiY2praGJibDFuMHNvZzN2bzNtcWZnbXhhcCJ9.xXYfoIoIpRaO4CXYrqywZw
-  mymap = L.map("map").setView([32.887890616465, -97.2487791627645], 9);
+  mymap = L.map("map").setView([32.97733373645308, -96.68380737304688], 10);
   L.tileLayer(
     "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
     {
@@ -14,6 +14,31 @@ $(document).ready(function() {
         "pk.eyJ1IjoiY2hsb2UtbWMiLCJhIjoiY2praGJibDFuMHNvZzN2bzNtcWZnbXhhcCJ9.xXYfoIoIpRaO4CXYrqywZw"
     }
   ).addTo(mymap);
+
+  var unt_marker = L.marker([33.207156, -97.152629])
+    .addTo(mymap)
+    .bindPopup(
+      "<h6>UNT</h6><ul>" +
+        "<li>Graduated Summa Cum Laude</li>" +
+        "<li>Majored in Geography</li>" +
+        "<li>GIS Certification</li></ul>"
+    );
+
+  var denton_marker = L.marker([33.201731, -97.087396])
+    .addTo(mymap)
+    .bindPopup("<h6>Denton County</h6>");
+
+  var le_marker = L.marker([33.158723, -96.939448])
+    .addTo(mymap)
+    .bindPopup("<h6>Town of Little Elm</h6>");
+
+  var td_marker = L.marker([32.932248, -97.110703])
+    .addTo(mymap)
+    .bindPopup("<h6>Templeton Demographics</h6>");
+
+  var tarrant_marker = L.marker([32.755129, -97.335246])
+    .addTo(mymap)
+    .bindPopup("<h6>Tarrant County</h6>");
 });
 
 //particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
