a5c65409c9ec9c9de71a3b3d4d2c35d6ffb35c63
Chloe McKnight <cbt0029@gmail.com>
2019-04-18 10:56:54 -0500

    adds screenshot carousel

--- scripts/main.js
+++ scripts/main.js
@@ -11,13 +11,13 @@ var experiences = [
         location: [33.201731, -97.087396],
         title: "Denton County",
         timespan: "SEPTEMBER 2012 - MAY 2013",
-        body: "I worked here as a GIS technician part-time while attending UNT. I became well acquainted with georeferencing while digitizing 1958 aerial imagery."
+        body: "I worked as a GIS technician part-time while attending UNT. I became well acquainted with georeferencing while digitizing 1958 aerial imagery."
     },
     {
         location: [33.158723, -96.939448],
         title: "Town of Little Elm",
         timespan: "MAY 2013 - AUGUST 2013",
-        body: "After graduation, I sought full-time employment at Little Elm as a GIS technician. Started experimenting with python and made some pretty maps."
+        body: "After graduation, I sought full-time employment at Little Elm as a GIS technician. I started experimenting with python and made useful and attractive maps."
     },
     {
         location: [32.932248, -97.110703],
@@ -31,7 +31,7 @@ var experiences = [
         title: "Tarrant County",
         timespan: "SEPTEMBER 2016 - PRESENT",
         body: "Itching for more time to program, I was happy to accept a full-time GIS developer position at Tarrant County. " +
-            "Here, I have expanded my technical skills and worked on the cool projects that you can check out below."
+            "Here, I have expanded my technical skills and worked on the cool projects listed in the next section." 
     }
 ];
 
