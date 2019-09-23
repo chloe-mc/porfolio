164ff682ffe5e6bbe3464b4f9721a2c919a4ca72
chlochlofasho <cbt0029@gmail.com>
2019-09-23 00:57:56 -0500

    remove public folder

--- public/styles-53d3aca786c25374bd25.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{240:function(n,o,w){},270:function(n,o,w){}}]);
-//# sourceMappingURL=styles-53d3aca786c25374bd25.js.map
--- public/styles-d14bfba323ac76c4df1b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{240:function(n,o,w){},270:function(n,o,w){}}]);
-//# sourceMappingURL=styles-d14bfba323ac76c4df1b.js.map
